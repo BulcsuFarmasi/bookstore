4af218583e1037efb8b2b19392176bec87d7b90a
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2018-12-19 23:47:44 +0100

    add get book and markup

--- src/app/modules/book/components/book/book.component.ts
+++ src/app/modules/book/components/book/book.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Book } from 'src/app/models/book';
+import { BookService } from 'src/app/services/book.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'book',
   templateUrl: './book.component.html',
   styleUrls: ['./book.component.scss']
 })
-export class BookComponent implements OnInit {
+export class BookComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  book:Book
+  private bookSubscription:Subscription;
+
+  constructor(private bookService:BookService, private route:ActivatedRoute)  { }
 
   ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+
+    this.bookSubscription = this.bookService.getBook(id).subscribe((book) => {
+        this.book = book;
+    })
+  }
+
+  ngOnDestroy () {
+    this.bookSubscription.unsubscribe();
   }
 
 }
