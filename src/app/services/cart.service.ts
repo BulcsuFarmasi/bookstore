ddb2cb45ab8a960c638f4beec6f4889f54e50626
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2018-12-19 22:50:48 +0100

    add addToCart and setCart

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 
 import { StorageService } from './storage.service';
 import { Cart } from '../models/cart';
+import { Book } from '../models/book';
 
 @Injectable()
 export class CartService {
@@ -9,10 +10,24 @@ export class CartService {
   private cart:Cart;
 
   constructor(private storageService:StorageService) {}
+  
+  addToCart(book:Book) {
+    this.cart.books.push(book);
+    this.setCart();
+  }
+  
+  getCart () {
+    this.loadCart();
+    return this.cart
+  }
 
-  loadCart () {
+  private loadCart () {
     if (!this.cart) {
       this.cart = this.storageService.get('cart');
     }
+  }
+
+  private setCart () {
+    this.storageService.set('cart', this.cart);
   }
 }
