1f5aa0b987164d767f1007086c9110bb92e4221e
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2018-12-19 23:37:25 +0100

    add BookComponent

--- src/app/modules/book/book.module.ts
+++ src/app/modules/book/book.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { BooksComponent } from './components/books/books.component';
 import { BookFilterComponent } from './components/book-filter/book-filter.component';
+import { BookComponent } from './components/book/book.component';
 
 @NgModule({
-  declarations: [BooksComponent, BookFilterComponent],
+  declarations: [BooksComponent, BookFilterComponent, BookComponent],
   imports: [
     CommonModule,
     FormsModule
