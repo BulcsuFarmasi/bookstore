2e11b94f6da1ec61e82b28c1ed1de25b5451ec65
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2018-12-19 23:53:03 +0100

    add book routing

--- src/app/modules/book/book.module.ts
+++ src/app/modules/book/book.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+
 import { BooksComponent } from './components/books/books.component';
 import { BookFilterComponent } from './components/book-filter/book-filter.component';
 import { BookComponent } from './components/book/book.component';
+import { BookRoutingModule } from './modules/book-routing/book-routing.module';
 
 @NgModule({
   declarations: [BooksComponent, BookFilterComponent, BookComponent],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    BookRoutingModule
   ]
 })
 export class BookModule { }
---
+++ src/app/modules/book/modules/book-routing/book-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { BooksComponent } from '../../components/books/books.component';
+import { BookComponent } from '../../components/book/book.component';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      { path: '/books', component: BooksComponent },
+      { path: '/book/:id', component: BookComponent  }
+    ])
+  ],
+  exports: [RouterModule]
+})
+export class BookRoutingModule { }
